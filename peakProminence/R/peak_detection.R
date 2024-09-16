38f7890fb7622763568a5fdefe6c488ba407ded0
Heba Aleterji <hebaaleterji@gmail.com>
2024-09-16 21:04:51 +1200

    Update: function loops through each element in a vector, checking if it's a local maximum by comparing it to its neighbors (left and right). Peaks must also meet the min_peak_height requirement.

--- peakProminence/R/peak_detection.R
+++ peakProminence/R/peak_detection.R
@@ -1,24 +1,34 @@
-#' Detect Peaks in 1D or 2D Data using pracma::findpeaks
+#' Custom Detect Peaks in 1D or 2D Data
 #'
-#' This function wraps the findpeaks() function from the pracma package to identify peaks in a numeric vector or matrix.
-#' For 2D data (matrix), it applies peak detection row-wise.
+#' This function detects peaks by identifying local maxima in a numeric vector or matrix.
+#' For 2D data (matrix), it applies peak detection row-wise or column-wise.
 #'
-#' @param data A numeric vector (1D) or matrix (2D) containing the data in which to detect peaks
-#' @param min_peak_height A numeric value for the minimum peak height (default is 0)
-#' @param apply_across Either "row" or "column" to specify if peaks should be detected row-wise or column-wise for 2D data (default is "row")
+#' @param data A numeric vector (1D) or matrix (2D) containing the data in which to detect peaks.
+#' @param min_peak_height A numeric value for the minimum peak height (default is 0).
+#' @param apply_across Either "row" or "column" to specify if peaks should be detected row-wise or column-wise for 2D data (default is "row").
 #' @return A list of data frames, each containing the positions and heights of detected peaks for each row or column in 2D data. For 1D data, returns a single data frame.
-#' @import pracma
 #' @export
 detect_peaks <- function(data, min_peak_height = 0, apply_across = "row") {
 
+  # Helper function to find peaks in 1D data
+  find_peaks_custom <- function(vec, min_peak_height) {
+    peak_positions <- c()
+    peak_heights <- c()
+
+    for (i in 2:(length(vec) - 1)) {
+      if (vec[i] > vec[i - 1] && vec[i] > vec[i + 1] && vec[i] >= min_peak_height) {
+        peak_positions <- c(peak_positions, i)
+        peak_heights <- c(peak_heights, vec[i])
+      }
+    }
+
+    return(data.frame(Position = peak_positions, Height = peak_heights))
+  }
+
   # Check if data is a numeric vector (1D) or matrix (2D)
   if (is.vector(data) && is.numeric(data)) {
-    # For 1D data, apply findpeaks directly
-    peaks <- pracma::findpeaks(data, minpeakheight = min_peak_height)
-    if (is.null(peaks)) {
-      return(data.frame(Position = integer(0), Height = numeric(0)))
-    }
-    return(data.frame(Position = peaks[, 2], Height = peaks[, 1]))
+    # For 1D data, apply custom peak detection
+    return(find_peaks_custom(data, min_peak_height))
 
   } else if (is.matrix(data) && is.numeric(data)) {
     # For 2D data (matrix), apply peak detection row-wise or column-wise
@@ -26,21 +36,11 @@ detect_peaks <- function(data, min_peak_height = 0, apply_across = "row") {
 
     if (apply_across == "row") {
       for (i in 1:nrow(data)) {
-        row_peaks <- pracma::findpeaks(data[i, ], minpeakheight = min_peak_height)
-        if (!is.null(row_peaks)) {
-          result_list[[i]] <- data.frame(Row = i, Position = row_peaks[, 2], Height = row_peaks[, 1])
-        } else {
-          result_list[[i]] <- data.frame(Row = i, Position = integer(0), Height = numeric(0))
-        }
+        result_list[[i]] <- find_peaks_custom(data[i, ], min_peak_height)
       }
     } else if (apply_across == "column") {
       for (i in 1:ncol(data)) {
-        col_peaks <- pracma::findpeaks(data[, i], minpeakheight = min_peak_height)
-        if (!is.null(col_peaks)) {
-          result_list[[i]] <- data.frame(Column = i, Position = col_peaks[, 2], Height = col_peaks[, 1])
-        } else {
-          result_list[[i]] <- data.frame(Column = i, Position = integer(0), Height = numeric(0))
-        }
+        result_list[[i]] <- find_peaks_custom(data[, i], min_peak_height)
       }
     } else {
       stop("Invalid option for 'apply_across'. Use 'row' or 'column'.")
@@ -50,5 +50,4 @@ detect_peaks <- function(data, min_peak_height = 0, apply_across = "row") {
   } else {
     stop("Input data must be either a numeric vector or matrix.")
   }
-
 }
