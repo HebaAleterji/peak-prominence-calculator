358b26b062e266ca586b51053b8d11c0b7213d2f
Heba Aleterji <hebaaleterji@gmail.com>
2024-09-16 21:08:25 +1200

    Update test file

--- peakProminence/tests/testthat/test-detect_peaks.R
+++ peakProminence/tests/testthat/test-detect_peaks.R
@@ -1,5 +1,5 @@
 test_that("detect_peaks correctly identifies peaks in 2D data", {
-  # Test data: a matrix with different peaks in rows
+  # Test with 2D data (matrix)
   data <- matrix(c(1, 3, 12, 6, 7, 10, 12, 5, 3,
                    2, 8, 10, 4, 5, 11, 14, 6, 1), nrow = 2, byrow = TRUE)
 
@@ -7,9 +7,9 @@ test_that("detect_peaks correctly identifies peaks in 2D data", {
   result_list <- detect_peaks(data, min_peak_height = 5, apply_across = "row")
 
   # Check if the function detects peaks in both rows correctly
-  expect_equal(result_list[[1]]$Position, c(3, 6))
-  expect_equal(result_list[[1]]$Height, c(12, 12))
+  expect_equal(result_list[[1]]$Position, c(3, 6)) # Expected positions for row 1
+  expect_equal(result_list[[1]]$Height, c(12, 12)) # Expected heights for row 1
 
-  expect_equal(result_list[[2]]$Position, c(2, 6, 7))
-  expect_equal(result_list[[2]]$Height, c(8, 11, 14))
+  expect_equal(result_list[[2]]$Position, c(2, 6, 7)) # Expected positions for row 2
+  expect_equal(result_list[[2]]$Height, c(8, 11, 14)) # Expected heights for row 2
 })
