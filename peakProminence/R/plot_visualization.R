94470ac27b631c1075680725b9ba87bf507ebc67
Heba Aleterji <hebaaleterji@gmail.com>
2024-09-19 21:08:20 +1200

    update plot_visualization() to plot data$peaks, data$ prominenece

--- peakProminence/R/plot_visualization.R
+++ peakProminence/R/plot_visualization.R
@@ -1,23 +1,34 @@
-
-
 #' Plot Peaks
-#' @param visualizer A PeakVisualizer object.
+#' @param object A PeakVisualizer object.
 #' @export
-plot_peaks.PeakVisualizer <- function(visualizer) {
-  plot(visualizer$data, type = "l", main = "Peaks", xlab = "Index", ylab = "Value")
-  points(visualizer$peaks$positions, visualizer$peaks$heights, col = "red", pch = 19)
+plot_peaks.PeakVisualizer <- function(object) {
+  data <- object$data
+  peaks <- object$peaks
+
+  # Plot the data
+  plot(data, type = "l", main = "Peak Detection", xlab = "Index", ylab = "Value")
+
+  # Highlight the peaks
+  points(peaks$positions, peaks$heights, col = "red", pch = 19)
 }
 
-
-#' Plot Prominences
-#' @param visualizer A PeakVisualizer object with prominences.
+#' Plot Prominence
+#' @param object A PeakVisualizer object.
 #' @export
-plot_prominence.PeakVisualizer <- function(visualizer) {
-  plot(visualizer$data, type = "l", main = "Peaks and Prominences", xlab = "Index", ylab = "Value")
-  points(visualizer$peaks$positions, visualizer$peaks$heights, col = "red", pch = 19)
+plot_prominence.PeakVisualizer <- function(object) {
+  data <- object$data
+  peaks <- object$peaks
+  prominences <- object$prominence
 
-  for (i in 1:length(visualizer$peaks$positions)) {
-    lines(c(visualizer$peaks$positions[i], visualizer$peaks$positions[i]),
-          c(visualizer$prominence$prominences[i], visualizer$peaks$heights[i]), col = "blue")
+  # Plot the data
+  plot(data, type = "l", main = "Prominence Detection", xlab = "Index", ylab = "Value")
+
+  # Highlight the peaks
+  points(peaks$positions, peaks$heights, col = "red", pch = 19)
+
+  # Add barplot for prominences
+  for (i in 1:length(peaks$positions)) {
+    segments(x0 = peaks$positions[i], y0 = peaks$heights[i] - prominences$prominences[i],
+             x1 = peaks$positions[i], y1 = peaks$heights[i], col = "blue", lwd = 2)
   }
 }
