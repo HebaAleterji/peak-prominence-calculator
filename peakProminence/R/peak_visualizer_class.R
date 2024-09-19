fbe615d1ad1948c3b591d1c183925d2e850e2d77
Heba Aleterji <hebaaleterji@gmail.com>
2024-09-19 14:42:22 +1200

    Update Visualizer class

--- peakProminence/R/peak_visualizer_class.R
+++ peakProminence/R/peak_visualizer_class.R
@@ -1,25 +1,11 @@
-# R/peak_visualizer_class.R
 
-#' Plot detected peaks using PeakVisualizer
-#' @param obj A PeakVisualizer object.
+
+#' PeakVisualizer class
+#' @param data A numeric vector representing the signal data.
+#' @param peaks A Peak object containing detected peaks.
+#' @param prominence (Optional) A Prominence object containing prominence values for the peaks.
+#' @return A PeakVisualizer object.
 #' @export
-plot_peaks.PeakVisualizer <- function(obj) {
-  if (is.null(obj$peaks)) stop("No peaks detected.")
-
-  # Plot the data
-  plot(obj$data, type = "l", main = "Detected Peaks", xlab = "Index", ylab = "Value")
-
-  # Plot the peaks using graphics::points to ensure correct import
-  graphics::points(obj$peaks$positions, obj$peaks$heights, col = "red", pch = 19)
-}
-
-#' Plot peak prominence using PeakVisualizer
-#' @param obj A PeakVisualizer object.
-#' @export
-plot_prominence.PeakVisualizer <- function(obj) {
-  if (is.null(obj$prominence)) stop("No prominence data.")
-
-  # Plot the prominence using graphics::barplot to ensure correct import
-  graphics::barplot(obj$prominence$prominences, names.arg = obj$prominence$positions,
-                    main = "Peak Prominence", xlab = "Position", ylab = "Prominence", col = "blue")
+PeakVisualizer <- function(data, peaks, prominence = NULL) {
+  structure(list(data = data, peaks = peaks, prominence = prominence), class = "PeakVisualizer")
 }
